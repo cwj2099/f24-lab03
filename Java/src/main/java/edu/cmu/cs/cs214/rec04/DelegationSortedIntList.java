6d5d0d751bd3a2242ad3370a786fd7f6a2f03599
cwj2099 <wc1629@nyu.edu>
2024-09-12 21:29:12 -0400

    Implemented Delegation sorted int list for Java
    Most changes happens in DeleationSortedIntList.java

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/DelegationSortedIntList.java
@@ -10,7 +10,54 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class DelegationSortedIntList {
-    // Write your implementation below with API documentation
+public class DelegationSortedIntList implements IntegerList{
+
+    //Most of the codes is just using a sortedIntlist
+    //However, add and add all is modified to record total added
+    SortedIntList myIntList = new SortedIntList();
+    int totalAdded = 0;
+
+    @Override
+    public boolean add(int num) {
+        boolean result = myIntList.add(num);
+        if(result) totalAdded ++;
+        return result;
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean result = myIntList.addAll(list);
+        if(result) totalAdded += list.size();
+        return result;
+    }
+
+    @Override
+    public int get(int index) {
+        return myIntList.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        return myIntList.remove(num);
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        return myIntList.removeAll(list);
+    }
+
+    @Override
+    public int size() {
+        return myIntList.size();
+    }
+
+    /**
+     * Return the total added
+     * @return
+     */
+    public int getTotalAdded() {
+        return totalAdded;
+    }
+
 
 }
